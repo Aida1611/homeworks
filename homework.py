57555077a298847f4b8fd72ac1fb906968aedd49
Aida1611 <aida241116@gmail.com>
2025-03-08 20:40:25 +0600

    Бул бутакта 07.03.2025 уй тапшырмасы сакталды

---
+++ homework.py
@@ -0,0 +1,9 @@
+name = "Aida"
+age = 36
+
+a = 34
+b = 43
+
+print(f'My name is {name}, I am {age} years old. {a} + {b} = {a + b} ')
+print(f'My name is {name.upper()}, I am {age} years old. {a} + {b} = {a + b} ')
+print(f'My name is {name.capitalize()}, I am {age} years old. {a} + {b} = {a + b} ')
