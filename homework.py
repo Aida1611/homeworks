ce583ebc749d6db5e78e5c408355e32f7ba2ebed
Aida1611 <aida241116@gmail.com>
2025-03-13 20:08:55 +0600

    Бул бутакта Тип set  сакталган

---
+++ homework.py
@@ -0,0 +1,23 @@
+##set-множество - коптуктор
+nums = {1,1,1,2,2,2,3,3}   ##окшош сандардан 1 ден чыгарат, порядок жокб индекс кошо албайбыз,узундугун текшерсе болот
+names = {
+    "Arsen",
+    "Grad",
+    "Shergazy",
+    "Ernis",
+    "Aida",
+    "Aiana",
+    "Begimay"
+}
+name = input("Enter a name: ")
+print(names)
+names.add(name)
+print(names)
+
+
+
+
+
+
+
+
