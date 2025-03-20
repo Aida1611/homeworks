ff30e74cdeb040dee04c807bbe1752fdd47933d5
Aida1611 <aida241116@gmail.com>
2025-03-20 19:37:49 +0600

    Бул ьутакта модуль

---
+++ homework.py
@@ -0,0 +1,21 @@
+import random
+import datetime
+
+while True:
+    a = random.randint (0, 100)
+    print(a)
+
+from random import randint #Импорт коркретных объектов из модуля
+x =randint (0, 10)
+
+import datetime as dt
+now = dt.datetime.now()   #Импорт всего мождуля с псевдонимом
+
+from random import  * #импорт всех объектов из модуля
+randint(0, 4)
+
+
+##random-cлучайные числа, другие
+
+
+
